d46bfbad9bc7a3b686ac92b524b6b2c4e82cba19
Vaibhav Bhagare <bhagare.vaibhav@gmail.com>
2022-08-10 01:26:21 +0530

    Suspense

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -3,6 +3,7 @@ import DashboardContainer from "../components/dashboard/DashboardContainer";
 import NavbarContainer from "../components/header/Navbar";
 import FeedbackContainer from "../components/feedback/FeedbackContainer";
 import TopicContainer from "../components/topic/TopicContainer";
+import { Suspense } from "react";
 
 const MainRoute = ({}) => {
   return (
@@ -10,9 +11,30 @@ const MainRoute = ({}) => {
       <BrowserRouter>
         <NavbarContainer />
         <Routes>
-          <Route path="/" element={<DashboardContainer />} />
-          <Route path="/feedback" element={<FeedbackContainer />} />
-          <Route path="/topic" element={<TopicContainer />} />
+          <Route
+            path="/"
+            element={
+              <Suspense fallback={"Loading..."}>
+                <DashboardContainer />
+              </Suspense>
+            }
+          />
+          <Route
+            path="/feedback"
+            element={
+              <Suspense fallback={"Loading..."}>
+                <FeedbackContainer />
+              </Suspense>
+            }
+          />
+          <Route
+            path="/topic"
+            element={
+              <Suspense fallback={"Loading..."}>
+                <TopicContainer />
+              </Suspense>
+            }
+          />
         </Routes>
       </BrowserRouter>
     </>
